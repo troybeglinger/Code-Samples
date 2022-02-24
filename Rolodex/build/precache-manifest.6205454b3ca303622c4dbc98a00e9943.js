87a652f81514e54c0f13e721400f3f1f53570177
Troy C. Beglinger <aeanos@yahoo.com>
2022-02-24 13:54:57 -0800

    Add files via upload

---
+++ Rolodex/build/precache-manifest.6205454b3ca303622c4dbc98a00e9943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea855c4e4fd50a7130e05bcd71440ba8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d7fb2b4551a0a4139edc",
+    "url": "/monsters-rolodex/static/css/main.55393372.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/monsters-rolodex/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fb2b4551a0a4139edc",
+    "url": "/monsters-rolodex/static/js/main.5848fa1d.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
