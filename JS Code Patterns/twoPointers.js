146e3a38e4fe6fbd707531bff2b5f6ce9c34590a
Troy Beglinger <troy@beglingerfineart.com>
2022-09-20 17:55:34 -0700

    updated twoPointers

--- JS Code Patterns/twoPointers.js
+++ JS Code Patterns/twoPointers.js
@@ -1,42 +1,13 @@
-<script>
-// Two pointer technique based solution to find if there is a pair in A[0..N-1] with a given sum.
-
-function isPairSum(A, N, X)
-{
- 
-    // represents first pointer
-    let i = 0;
- 
-    // represents second pointer
-    let j = N - 1;
- 
-    while (i < j) {
-        // If we find a pair
-        if (A[i] + A[j] == X)
-            return true;
- 
-        // If sum of elements at current pointers is less, we move towards higher values by doing i++
-        else if (A[i] + A[j] < X)
-            i++;
- 
-        // If sum of elements at current pointers is more, we move towards lower values by doing j--
-        else
-            j--;
-    }
-
+const uniqueValueCount = function (array) {   if (array.length < 1) {
     return false;
-}
- 
-    // Array declaration
-    let arr = [ 3, 5, 9, 2, 8, 10, 11 ];
-     
-    // Value to search for
-    let val = 17;
-     
-    // Size of the array
-    let arrSize =7;
-     
-    // Function call
-    document.write(isPairSum(arr, arrSize, val));
-
-   </script>
+  }
+  
+  let pointerOne = 0;
+  let pointerTwo = pointerOne + 1;
+  let uniqueValues = 1;    while (pointerTwo < array.length) {   if (array[pointerOne] === array[pointerTwo]) {
+     pointerTwo++;   }  else {
+     pointerOne = pointerTwo;
+     uniqueValues++;
+   }  }
+return uniqueValues;};
+console.log(uniqueValueCount([2, 3, 4, 4]));
