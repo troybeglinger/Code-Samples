ca93ac3b25693a672c448f0b036929342666a53d
Troy Beglinger <troy@beglingerfineart.com>
2022-09-20 18:02:02 -0700

    updated twoPointers

--- JS Code Patterns/twoPointers.js
+++ JS Code Patterns/twoPointers.js
@@ -11,3 +11,9 @@ const uniqueValueCount = function (array) {   if (array.length < 1) {
    }  }
 return uniqueValues;};
 console.log(uniqueValueCount([2, 3, 4, 4]));
+
+// Check if array is empty. If so, return false.
+// Define pointers that will each refer to an index. First value plus adjacent scout number.
+// Define uniqueValues starting at 1, since there will always be at least one.
+// Use a while loop to let the second pointer do some logic until the scout pointer reaches the end of the array.
+// If the two pointers start at the same value, move the scout up one.
