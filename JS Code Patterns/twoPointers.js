f376e0336d040d8881be469c1a4b9ad303dc6718
Troy Beglinger <troy@beglingerfineart.com>
2022-09-20 18:03:17 -0700

    updated twoPointers

--- JS Code Patterns/twoPointers.js
+++ JS Code Patterns/twoPointers.js
@@ -12,7 +12,8 @@ const uniqueValueCount = function (array) {   if (array.length < 1) {
 return uniqueValues;};
 console.log(uniqueValueCount([2, 3, 4, 4]));
 
-// Check if array is empty. If so, return false.
+// Problem - Write a function called countUniqueValues, that accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+// Solution - Check if array is empty. If so, return false.
 // Define pointers that will each refer to an index. First value plus adjacent scout number.
 // Define uniqueValues starting at 1, since there will always be at least one.
 // Use a while loop to let the second pointer do some logic until the scout pointer reaches the end of the array.
